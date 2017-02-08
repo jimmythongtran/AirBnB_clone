f5265783d07bb45debc255eb9617b2c06483e90e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-08 01:43:32 +0000

    Pep8 checked

--- models/base_model.py
+++ models/base_model.py
@@ -13,8 +13,8 @@ class BaseModel:
     This is the class for BaseModel
     """
     def __init__(self):
-       self.id = uuid.uuid4()
-       self.created_at = datetime.datetime.now()
+        self.id = uuid.uuid4()
+        self.created_at = datetime.datetime.now()
 
     def __str__(self):
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
