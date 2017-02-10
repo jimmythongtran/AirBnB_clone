29a44a5cfb398f8655cfe8c4448f7d28341debac
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-10 23:06:50 +0000

    .ids may be issue

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """User inherits from BaseModel"""
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        place_id = "" # TODO: it will be the Place.id
+        user_id = "" # TODO: it will be the User.id
+        text = ""
