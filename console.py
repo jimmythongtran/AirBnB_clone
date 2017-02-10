79784e81d5407f34ffc34678e7b120c3f2df37cf
j-tyler <justin.marsh@holbertonschool.com>
2017-02-10 18:54:06 +0000

    Add do_create to console

--- console.py
+++ console.py
@@ -2,11 +2,26 @@
 
 import cmd
 import sys
+from models.base_model import BaseModel
 
 
 class ShellPrompt(cmd.Cmd):
-    intro = 'Welcome to the HBnB shell. Type help or ? to list commands.\n'
+    intro = 'Welcome to the HBnB shell. Type help or ? to list commands.'
     prompt = '(hbnb) '
+
+    def do_create(self, args):
+        new = BaseModel()
+        new.save()
+        print("{}".format(new.id))
+
+    def do_destroy(self, args):
+        print("We are destroying")
+
+    def do_all(self, args):
+        print("We are in all")
+
+    def do_update(self, args):
+        print("We are updating")
 
     def do_quit(self, args):
         """Quits the program."""
@@ -14,7 +29,8 @@ class ShellPrompt(cmd.Cmd):
 
     def do_EOF(self, args):
         """Reached EOF"""
-        return True
+        print('')
+        raise SystemExit
 
 if __name__ == '__main__':
     ShellPrompt().cmdloop()
