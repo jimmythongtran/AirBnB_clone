58db81bc9be92a7885d545c64cf9b72002ee688a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-10 19:41:48 +0000

    Added args kwargs

--- models/user.py
+++ models/user.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
+#TODO: NameError: name 'BaseModel is not defined'
 class User(BaseModel):
-"""
-User inherits from BaseModel
-"""
-    def __init__(self):
-
+    """User inherits from BaseModel"""
+    def __init__(self, *args, **kwargs):
         email = ""
         password = ""
         first_name = ""
