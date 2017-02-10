fc72b3775b8c8ea7ac07971ba78479add2579485
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-10 07:13:09 +0000

    Untested

--- models/base_model.py
+++ models/base_model.py
@@ -13,23 +13,22 @@ class BaseModel:
     This is the class for BaseModel
     """
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+        # self.id = str(uuid.uuid4())
+        # self.created_at = datetime.datetime.now()
         # check this
         if isinstance(args, dict):
             self.__dict__ = args
         else:
             storage.new(self)
-          #  self.id = str(uuid.uuid4())
-          #  self.created_at = datetime.datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
 
     def __str__(self):
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
-       # check this
-       storage.save()
+        storage.save()
 
     def to_json(self):
         self.__dict__.update({'__class__': "BaseModel"})
