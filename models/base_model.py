e0f68a284e44514bdca1e95f408ec4000dd30bd3
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-10 02:48:56 +0000

    In base model, moved to else

--- models/base_model.py
+++ models/base_model.py
@@ -13,12 +13,14 @@ class BaseModel:
     This is the class for BaseModel
     """
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+        # self.id = str(uuid.uuid4()
+        # self.created_at = datetime.datetime.now()
         if isinstance(args, dict):
             self.__dict__ = args
         else:
             storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
 
     def __str__(self):
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
