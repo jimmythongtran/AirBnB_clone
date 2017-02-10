e34da645247a61ce4e0ae65de688fb579c91c848
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-10 20:14:31 +0000

    Added self but no attribute error persists

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User inherits from BaseModel"""
     def __init__(self, *args, **kwargs):
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
