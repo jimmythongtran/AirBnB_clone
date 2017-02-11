dc4d118bca13e404df55cb43fcac85df7bcb7c12
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-11 00:21:33 +0000

    Line 19 empty string list maybe wrong

--- models/place.py
+++ models/place.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+
 class Place(BaseModel):
     """User inherits from BaseModel"""
     def __init__(self, *args, **kwargs):
         super().__init__()
-        city_id = "" # TODO: it will be the City.id
-        user_id = "" # TODO: it will be the User.id
+        city_id = ""  # TODO: it will be the City.id
+        user_id = ""  # TODO: it will be the User.id
         name = ""
         description = ""
-        number_rooms: 0 # integer - 0
-        number_bathrooms: 0 # integer - 0
-        max_guest: 0 # integer - 0
-        price_by_night: # integer - 0
-        latitude: 0.0 # float - 0.0
-        longitude: 0.0 # float - 0.0
-        amenities: [""] # list of string - empty list: TODO: it will be the list of Amenity.id
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenities = [""]   # TODO: it will be the list of Amenity.id
