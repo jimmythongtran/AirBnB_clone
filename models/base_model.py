23de8249f082d4925ce798eef51de20c221d5d0e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-02-11 00:07:39 +0000

    Changed back to original format

--- models/base_model.py
+++ models/base_model.py
@@ -22,8 +22,7 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
