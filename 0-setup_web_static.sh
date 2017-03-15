7e891b7a0671487b407c8167958498b8d4c3b74e
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 20:47:56 +0000

    Sed changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ sudo mkdir -p /data/web_static/releases/test/index.html
 sudo mkdir -p /data/web_static/shared/
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "4i location {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/nginx.conf
+sudo sed -i "89i location {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/sites-enabled/default
 sudo echo "Holberton" > /data/web_static/releases/test/index.html
 sudo service nginx restart
