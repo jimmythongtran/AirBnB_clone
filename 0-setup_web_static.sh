29deb3dbaedf039e77ea7b2b178a55bc2feea6d4
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 20:37:19 +0000

    moved symlinks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get install nginx
 sudo apt-get install curl
 sudo mkdir -p /data/web_static/releases/test/index.html
 sudo mkdir -p /data/web_static/shared/
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data/
 sed -i "4i location {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/nginx.conf
 echo "Holberton" > /data/web_static/releases/test/index.html
