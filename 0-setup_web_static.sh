3894e353f5f9de756713ec00effec8f9ef4b11dd
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 20:42:16 +0000

    sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # this sets up web servers for deployment
 sudo apt-get update
-sudo apt-get install nginx
-sudo apt-get install curl
+sudo apt-get install -y nginx
+sudo apt-get install -y curl
 sudo mkdir -p /data/web_static/releases/test/index.html
 sudo mkdir -p /data/web_static/shared/
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
-sed -i "4i location {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/nginx.conf
-echo "Holberton" > /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "4i location {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/nginx.conf
+sudo echo "Holberton" > /data/web_static/releases/test/index.html
 sudo service nginx restart
