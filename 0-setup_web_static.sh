acfdc2e8c5593fd6e8aad8bc5a1addb1fadd7475
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 20:50:12 +0000

    shellcheck sudo tee

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ sudo mkdir -p /data/web_static/shared/
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "89i location {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/sites-enabled/default
-sudo echo "Holberton" > /data/web_static/releases/test/index.html
+echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
 sudo service nginx restart
