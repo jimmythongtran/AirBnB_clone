05917b7362c8bdd0cf49922321a990f68ae764c3
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 21:05:41 +0000

    removed index.html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y nginx
 sudo apt-get install -y curl
-sudo mkdir -p /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
