4b5112acd91f1a879a3fed510ae6c81769c4313b
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 23:50:38 +0000

    local import

---
+++ 1-pack_web_static.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# this generates .tgz archive from contents of web_static folder
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    file = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    local('tar -cvzf versions/web_static_%s.tgz" web_static' % file)
