742060c20d39b4074fe8ca8445ba4a44a6e69ad0
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 22:01:39 +0000

    ordered index.html up

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "37i\ \n\n\tlocation /hbnb_static/ {\n\talias\t/data/web_static/current/;\n\tautoindex off;\n}" /etc/nginx/sites-enabled/default
-echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
 sudo service nginx restart
