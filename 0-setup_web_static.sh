b75356358949f3eb162d39a614086e7454486ec3
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-15 21:03:21 +0000

    sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ sudo mkdir -p /data/web_static/releases/test/index.html
 sudo mkdir -p /data/web_static/shared/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "37i \tlocation /hbnb_static {\n\talias\t/data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo sed -i "37i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-enabled/default
 echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
 sudo service nginx restart
